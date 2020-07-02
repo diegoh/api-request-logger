3d26f8c5e5c4a89f7feda8cc2fcb0aa9f70aa3f0
Diego Herrera <608492+diegoh@users.noreply.github.com>
2020-07-02 15:42:10 +0100

    Ignore dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { DefaultContext, DefaultState, ParameterizedContext } from 'koa';
-import { Logger } from './logger.interface';
-declare const _default: (logger: Logger) => (ctx: ParameterizedContext<DefaultState, DefaultContext>, error: Error) => Promise<void>;
-export default _default;
--- dist/logger.interface.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Logger {
-    error(logmessage: string, logPayload: string): void;
-    info(logmessage: string, logPayload: string): void;
-}
