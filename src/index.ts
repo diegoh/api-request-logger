57fdf2bb2ff8c12e89159c4d314e316593e49e2e
Diego Herrera <608492+diegoh@users.noreply.github.com>
2020-07-05 17:27:00 +0100

    Fix exports (#28)
    * Fix exports
    
    * 4.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-import errorLogger from './error-logger';
-import infoLogger from './request-logger';
-
-export default { errorLogger, infoLogger };
+export * as errorLogger from './error-logger';
+export * as requestLogger from './request-logger';
